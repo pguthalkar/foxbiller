17bc073a6aa688095093afa74fc145bc94068149
Pratik Guthalkar <pguthalkar@gmail.com>
2019-07-29 22:57:25 +0530

    resolve conflict

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyB095W6iOhRwKVahKhWpWqT_6vO4MM5z48',
+    authDomain: 'angular-firebase-b8ac9.firebaseapp.com',
+    databaseURL: 'https://angular-firebase-b8ac9.firebaseio.com',
+    projectId: 'angular-firebase-b8ac9',
+    storageBucket: 'angular-firebase-b8ac9.appspot.com',
+    messagingSenderId: '621224926934'
+  }
+};
